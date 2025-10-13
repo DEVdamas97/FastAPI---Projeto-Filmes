b8f4dca05e1cad08e20cf5d0681546d6cc4d0524
Vinicius Damas <viniciusdamas97@gmail.com>
2025-10-13 10:38:53 -0300

    rota listar filmes

--- Back-End/api.py
+++ Back-End/api.py
@@ -25,3 +25,19 @@ def home():
     return {
         "mensagem": "Bem-Vindo ao gerenciador de filmes!"
     }
+
+@app.get("/filmes")
+def catalogo():
+    filmes = funcao.listar_movies()
+    lista = []
+    for linha in filmes:
+        lista.append({
+            "id": linha[0],
+            "titulo": linha[1],
+            "genero": linha[2],
+            "ano": linha[3],
+            "avaliacao": linha[4]
+        })
+    return {
+        "filmes": lista
+    }
