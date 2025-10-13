c325880d3411a5a5cf8ff55a0f314c39e4529b24
Vinicius Damas <viniciusdamas97@gmail.com>
2025-10-13 11:30:35 -0300

    Front-End - catalogo ✔

--- Front-End/app.py
+++ Front-End/app.py
@@ -1,2 +1,34 @@
+import streamlit as st
 
-# pip install fastapi uvicorn streamlit requests psycopg2 dotenv
+# Rodar o streamlit
+# python -m streamlit run app.py
+
+import requests 
+
+# Criar minha rota de api
+# URL da api do FastAPI
+API_URL = "http://127.0.0.1:8000"
+
+st.set_page_config(page_title="(☞ﾟヮﾟ)☞ 🎬 ", page_icon="🍿")
+
+st.title(" Gerenciador de Filmes")
+
+# Menu lateral sidebar
+
+menu = st.sidebar.radio("Navegação", ["Catalogo"])
+
+if menu == "Catalogo":
+    st.subheader("Todos os Filmes 📽")
+    respose = requests.get(f"{API_URL}/filmes")
+    if respose.status_code == 200:
+        filmes = respose.json().get("filmes", [])
+        if filmes:
+            for filme in filmes:
+                st.write(
+                    f"🎬 **{filme['titulo']}** "
+                    f"({filme['ano']})  \n"
+                    f"📌 Gênero: {filme['genero']}  \n"
+                    f"⭐ Avaliação: {filme.get('avaliacao', 'N/A')}"
+                )
+    else:
+        st.error("Erro ao cone")
